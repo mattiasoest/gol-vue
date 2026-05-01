5f2ecf9f81c67f7006084ec8762c89a596125aad
Matt <osth.mattias@gmail.com>
2026-05-01 15:21:17 +0900

    remove router

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { createPinia } from 'pinia'
 
 import './assets/base.css'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
